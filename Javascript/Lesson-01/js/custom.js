fee79e8ae84df0ef10b609cd55d62f5cc8b25aac
Luana <luanacamm@users.noreply.github.com>
2020-08-31 20:10:55 -0300

    Loops in Javascript

---
+++ Javascript/Lesson-01/js/custom.js
@@ -0,0 +1,30 @@
+a = 5;
+b = '5';
+
+if (a == b) {
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+//While loop
+var a = 0;
+while (a < 10){
+    console.log(a + 'Hello Wold');
+    a++;
+}
+
+var a = 0;
+do{
+    console.log(a +' Bye World');
+    a++;
+}while
+    (a < 10);
+
+for(i = 1; i< 20; i++){
+    console.log(i+ 'Hi There!')
+    if(i==6){
+        console.log('We have reached 6!');
+        continue;
+    }
+}
